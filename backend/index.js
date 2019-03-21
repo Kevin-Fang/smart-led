adc6230f569d275ca414d1e06b61bad94249c124
kfang <kfang1233@gmail.com>
2019-03-21 10:12:16 -0400

    Updated backend - added animatinos

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 var express = require('express')
 var http = require('http')
 var socketio = require('socket.io')
+var ip = require("ip");
 
 var app = express()
 var server = http.Server(app)
 var io = socketio(server)
 
 let port = 8000
+
+let pattern;
 
 let testing = true;
 
@@ -26,12 +29,29 @@ let current = {
 }
 
 server.listen(port, () => {
-	console.log(`Listening on port ${port}`)
+	console.log(`Listening on ${ip.address()}:${port}`)
 })
 
+allOff = () => {
+	writeColor('red', 0)
+	writeColor('green', 0)
+	writeColor('blue', 0)
+	clearInterval(pattern)
+}
+
+let writeColor = (led, value) => {
+	if (!testing) {
+		leds[led].pwmWrite(value)
+	} else {
+		console.log(`Writing ${value} to ${led}`)
+	}
+	current[led] = value
+	if (!pattern) {
+		io.emit('colors', current)
+	}	
+}
 
 io.on('connection', (socket) => {
-	console.log("Client joined on", socket)
 	socket.emit('colors', current)
 
 	socket.on('setcolor', (colors) => {
@@ -46,14 +66,55 @@ io.on('connection', (socket) => {
 			writeColor('blue', colors.blue)
 		}
 	})
-})
 
-let writeColor = (led, value) => {
-	if (!testing) {
-		leds[led].pwmWrite(value)
-	} else {
-		console.log(`Writing ${value} to ${led}`)
-	}
-	current[led] = value
-	io.emit('colors', current)
-}
+	socket.on('police', () => {
+		allOff()
+		let red = true
+		pattern = setInterval(() => {
+			if (red) {
+				writeColor('red', 0)
+				writeColor('blue', 150)
+				red = false
+			} else {
+				writeColor('blue', 0)
+				writeColor('red', 150)
+				red = true
+			}}, 75)
+			
+		console.log("Police")
+	})
+
+	socket.on('fade', () => {
+		allOff()
+		let inc_led = "red"
+		let dec_led = null
+		let dutyCycle = 0
+
+		pattern = setInterval(() => {
+			dutyCycle = dutyCycle % 255
+			writeColor(inc_led, dutyCycle)
+			if (dec_led != null) {
+				writeColor(dec_led, 255 - dutyCycle)
+			}
+			dutyCycle += 3
+
+			if (dutyCycle >= 255) {
+				if (inc_led == "red") {
+					inc_led = "green"
+					dec_led = "red"
+				} else if (inc_led == "green") {
+					inc_led = "blue"
+					dec_led = "green"
+				} else if (inc_led == "blue") {
+					inc_led = "red"
+					dec_led = "blue"
+				}
+			}
+		}, 30)
+	})
+
+	socket.on('off', () => {
+		console.log("Turning everything off")
+		allOff()
+	})
+})
